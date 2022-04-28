3f5eb1d50760baba53a97d62189dd90031a8af76
呂健羽 <goustx@gmail.com>
2022-04-28 11:36:06 +0800

    test: RxJS 模式!?

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,30 +1,10 @@
-import { Controller, Post, Body, Get, Param, HttpCode, HttpStatus, Header } from '@nestjs/common';
-import { CreateTodoDto } from "./DTO/create-todo.dto"
+import { Controller, Get } from '@nestjs/common';
+import { of } from 'rxjs';
+
 @Controller('todos')
 export class TodoController {
-  // @Get(':id')
-  // @HttpCode(HttpStatus.NO_CONTENT)
-  // get(@Param('id') id: string) {
-  //   return {
-  //     id,
-  //     title: `Title ${id}`,
-  //     description: ''
-  //   };
-  // }
-
-  @Post()
-  create(@Body() dto: CreateTodoDto) {
-    const id = 1;
-    return { id, ...dto };
-  }
-
   @Get()
-  @Header('X-Hao-headers', '1')
   getAll() {
-    return {
-      id: 1,
-      title: 'Title 1',
-      description: 'header test'
-    };
+    return of([]);
   }
 }
