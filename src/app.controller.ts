b5e8a374c36c2d5e4605e42e1beb6d0a6e866b56
呂健羽 <goustx@gmail.com>
2022-04-28 17:35:19 +0800

    test: 例外處理的測試

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Inject, Optional } from '@nestjs/common';
+import { BadRequestException, Controller, Get, Inject, Optional } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CustomException } from './exceptions/custom.exception';
 
 @Controller()
 export class AppController {
@@ -13,6 +14,9 @@ export class AppController {
 
   @Get()
   getHello(): string {
+    // throw new BadRequestException('出錯囉!');//內部定義 400 錯誤
+    // throw new BadRequestException({ msg: '出錯囉!' }); //物件方式
+    throw new CustomException(); //拋出自定義異常
     return this.appService.getHello();
   }
 }
