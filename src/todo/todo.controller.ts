0acdffcab05ed7f5931b865b8f7f27c9dec4f544
呂健羽 <goustx@gmail.com>
2022-04-28 10:30:24 +0800

    test: add post method body test

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,13 +1,23 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, HttpCode, HttpStatus } from '@nestjs/common';
 
 @Controller('todos')
 export class TodoController {
   @Get(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
   get(@Param('id') id: string) {
     return {
       id,
       title: `Title ${id}`,
       description: ''
     };
+  }
+
+  @Post()
+  create(
+    @Body('title') title: string,
+    @Body('description') description?: string
+  ) {
+    const id = 1;
+    return { id, title, description };
   }
 }
