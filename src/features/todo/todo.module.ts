2b9e6d2d30a201b4b279fdc8e8b8b7982d89bc2c
呂健羽 <goustx@gmail.com>
2022-04-28 15:20:00 +0800

    test: provider 展開 自定義用法

--- src/features/todo/todo.module.ts
+++ src/features/todo/todo.module.ts
@@ -4,7 +4,15 @@ import { TodoService } from './todo.service';
 
 @Module({
   controllers: [TodoController],
-  providers: [TodoService],
+  providers: [
+    { //展開式用法
+      provide: TodoService,
+      useClass: TodoService //指定使用的class物件
+      // 如 useClass:{ name: 'HAO' }
+      // 或是 useClass:'HAO'
+      // 可以使用的有class、Object、string、symbol、enum
+    }
+  ],
   exports: [TodoService],
 })
 export class TodoModule {}
