e13b811dd551a38a98025d3bae20b5f1783c7386
KimLyu <goustx@gmail.com>
2022-04-29 14:38:51 +0800

    test:  ParseIntPipe 驗證功能測試

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,22 +1,12 @@
-import { BadRequestException, Controller, Get, Inject, Optional } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { AppService } from './app.service';
-import { CustomException } from './exceptions/custom.exception';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly appService: AppService, 
-    @Optional() @Inject('HANDSOME_MAN') private readonly handsome_man = { name: 'defaultKim' }
-    //使用Inject 將自定義的 HANDSOME_MAN 注入，若沒有ˊ注入，則使用預設值
-  ) {
-    console.log(handsome_man); //將注入資訊印到console
-  }
+  constructor(private readonly appService: AppService) { }
 
-  @Get()
-  getHello(): string {
-    // throw new BadRequestException('出錯囉!');//內部定義 400 錯誤
-    // throw new BadRequestException({ msg: '出錯囉!' }); //物件方式
-    throw new CustomException(); //拋出自定義異常
-    return this.appService.getHello();
+  @Get(':id')
+  getUser(@Param('id', ParseIntPipe) id: number) { //轉換時 插入ParseIntPipe
+    return this.appService.getUser(id);
   }
 }
