6064da411b743aee69746dbbd56a9fb10ca1935e
KimLyu <goustx@gmail.com>
2022-04-29 17:33:30 +0800

    test: DTO 錯誤訊息顯示、非定義參數部接收、無效參數報錯等設定。

--- src/feature/todo/todo.controller.ts
+++ src/feature/todo/todo.controller.ts
@@ -2,7 +2,13 @@ import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common
 import { CreateTodoDto } from './dto/create-todo.dto';
 
 @Controller('todos')
-@UsePipes(new ValidationPipe({ disableErrorMessages: true })) // 也可放在class層級 使整個Class都支持驗證
+  @UsePipes(
+    new ValidationPipe({
+      // disableErrorMessages: true, //不顯示錯誤訊息
+      whitelist: true, //非定義參數 不記錄近來
+      forbidNonWhitelisted: true //無效參數 報錯
+    })
+  )
 export class TodoController {
 
   @Post()
