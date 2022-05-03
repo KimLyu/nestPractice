fd55b6b6789bcab7418b06978a0f4b3c0bbec361
KimLyu <goustx@gmail.com>
2022-05-03 14:56:03 +0800

    test:  Array Pipe用法

--- src/feature/todo/todo.controller.ts
+++ src/feature/todo/todo.controller.ts
@@ -1,23 +1,15 @@
-import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, ParseArrayPipe, Post } from '@nestjs/common';
 import { CreateTodoDto } from './dto/create-todo.dto';
 
 @Controller('todos')
-  @UsePipes(
-    new ValidationPipe({
-      // disableErrorMessages: true, //不顯示錯誤訊息
-      whitelist: true, //非定義參數 不記錄近來
-      forbidNonWhitelisted: true //無效參數 報錯
-    })
-  )
 export class TodoController {
 
   @Post()
-  // @UsePipes(ValidationPipe) //需要裝飾符 引入pipe
-  create(@Body() dto: CreateTodoDto) {
-    return {
-      id: 1,
-      ...dto
-    };
+  create(
+    @Body(new ParseArrayPipe({ items: CreateTodoDto }))//Array的內容對應到DTO
+    dtos: CreateTodoDto[]
+  ) {
+    return dtos;
   }
 
 }
