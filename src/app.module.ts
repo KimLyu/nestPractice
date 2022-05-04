3d4da39a1202192d652a3332c2e06b3db2f31134
KimLyu <goustx@gmail.com>
2022-05-04 16:06:10 +0800

    test: forRoute 指定 路由 以及 http method方法

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './features/todo/todo.module';
@@ -13,6 +13,9 @@ import { LoggerMiddleware } from './middlewares/logger.middleware';
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes('/todos')
+    consumer.apply(LoggerMiddleware).forRoutes(
+      { path: '/todos', method: RequestMethod.POST },// POST /todos 會生效
+      { path: '/', method: RequestMethod.GET }// GET / 會生效
+    )
   }
 }
