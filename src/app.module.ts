749a30af26d77aca8f6c9a3a3476dafacd4ed1f3
KimLyu <goustx@gmail.com>
2022-05-04 16:12:01 +0800

    test: 使用多組中間件的方法

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoController } from './features/todo/todo.controller';
 import { TodoModule } from './features/todo/todo.module';
+import { HelloWorldMiddleware } from './middlewares/hello-world.middleware';
 import { LoggerMiddleware } from './middlewares/logger.middleware';
 
 @Module({
@@ -14,6 +15,7 @@ import { LoggerMiddleware } from './middlewares/logger.middleware';
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes(TodoController)
+    //在controller 指定使用兩個中間件
+    consumer.apply(LoggerMiddleware, HelloWorldMiddleware).forRoutes(TodoController) 
   }
 }
