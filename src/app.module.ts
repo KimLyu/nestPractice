baf51e81618c456efeafc9e64a9e2e6f1ac6a15b
KimLyu <goustx@gmail.com>
2022-05-04 16:08:06 +0800

    test: forRoute 指定controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
-import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TodoController } from './features/todo/todo.controller';
 import { TodoModule } from './features/todo/todo.module';
 import { LoggerMiddleware } from './middlewares/logger.middleware';
 
@@ -13,9 +14,6 @@ import { LoggerMiddleware } from './middlewares/logger.middleware';
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes(
-      { path: '/todos', method: RequestMethod.POST },// POST /todos 會生效
-      { path: '/', method: RequestMethod.GET }// GET / 會生效
-    )
+    consumer.apply(LoggerMiddleware).forRoutes(TodoController)
   }
 }
